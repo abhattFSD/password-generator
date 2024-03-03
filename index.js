4e016509b85e6093918644e0cd57ef59a3219000
abhattFSD <161577240+abhattFSD@users.noreply.github.com>
2024-03-03 08:41:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passOneEl = document.getElementById("pass1");
+let passTwoEl = document.getElementById("pass2");
+
+
+function generatePassoword() {
+    let pass = "";
+    for (let i = 1; i <= 12; i++) {
+        let char = Math.floor(Math.random()
+            * characters.length);
+ 
+        pass += characters[char]
+    }
+    
+    return pass;
+}
+
+function generateRandowmPassoword() {
+    let randomPassOne = generatePassoword();  
+    let randomPassTwo = generatePassoword(); 
+    
+    passOneEl.textContent = randomPassOne;
+    passTwoEl.textContent = randomPassTwo;
+}
+
